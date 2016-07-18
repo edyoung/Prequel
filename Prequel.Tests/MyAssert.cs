32882fd842ce4f512a94c1b0bedb8d27277fc524
Edwin Young <edyoung@microsoft.com>
2016-07-17 22:09:53 -0700

    interim

--- Prequel.Tests/MyAssert.cs
+++ Prequel.Tests/MyAssert.cs
@@ -4,14 +4,30 @@ namespace Prequel.Tests
 {
     using System.Linq;
     using Xunit;
+    using FluentAssertions;
+    using FluentAssertions.Execution;
 
     public static class MyAssert
     {
         public static void HaveNoErrorsOrWarnings(this CheckResults results)
         {
-            Assert.Empty(results.Errors);
-            Assert.Empty(results.Warnings);
-            Assert.Equal(ExitReason.Success, results.ExitCode);
+            Execute
+                .Assertion
+                .ForCondition(results.Errors.Count == 0)
+                .BecauseOf("checking this sql should not report any errors")
+                .FailWith("Found {0} errors, the first one is '{1}'", results.Errors.Count, results.Errors.FirstOrDefault());
+
+            Execute
+                .Assertion
+                .ForCondition(results.Warnings.Count == 0)
+                .BecauseOf("checking this sql should not report any warnings")
+                .FailWith("Found {0} warnings, the first one is '{1}'", results.Warnings.Count, results.Warnings.FirstOrDefault());
+
+            Execute
+                .Assertion
+                .ForCondition(results.ExitCode == ExitReason.Success)
+                .BecauseOf("if there are no errors or warnings, checking should be successful")
+                .FailWith($"Exit code was {results.ExitCode}");
         }
 
         public static void NoErrors(this CheckResults results)
